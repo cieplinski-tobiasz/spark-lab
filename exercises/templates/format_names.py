444211dfec243c82fbc9acb4406cf4f00bbc7ac4
Tobiasz Ciepliński <cieplinski.t@gmail.com>
2019-06-11 11:17:50 +0200

    Change format names' dataset

--- exercises/templates/format_names.py
+++ exercises/templates/format_names.py
@@ -3,7 +3,7 @@ from pyspark import SparkConf, SparkContext
 if __name__ == '__main__':
     conf = SparkConf().setMaster('local').setAppName('longest pt mention')
     sc = SparkContext(conf=conf)
-    names = ['andy', 'jOhN', 'Peter', 'MiCHAel', '36453', '#@##']
+    names = ['andy', 'jOhN', 'Peter', 'MiCHAel', '36453', '#@##', 'andy']
 
     names_rdd = sc.parallelize(names)
 
